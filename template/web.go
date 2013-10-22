8433e9c5135e1b0b2b5e30ee9226843239a45e7d
Vincent Batts <vbatts@redhat.com>
2013-10-22 13:25:43 -0400

    hello world to be more informative

--- template/web.go
+++ template/web.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "os"
+	"fmt"
+	"net/http"
+	"os"
+	"runtime"
 )
 
 func main() {
-  http.HandleFunc("/", hello)
-  fmt.Println("listening...")
-  err := http.ListenAndServe(os.Getenv("HOST")+":"+os.Getenv("PORT"), nil)
-  if err != nil {
-    panic(err)
-  }
+	http.HandleFunc("/", hello)
+	bind := fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
+	fmt.Printf("listening on %s...", bind)
+	err := http.ListenAndServe(bind, nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
-  fmt.Fprintln(res, "hello, world")
+	fmt.Fprintf(res, "hello, world from %s", runtime.Version())
 }
